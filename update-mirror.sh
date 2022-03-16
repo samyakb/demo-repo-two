7cf284a2305aa01c6344f8ece1148b75bdbbaf57
Samyak Bakliwal <99634190+samyakb@users.noreply.github.com>
2022-03-16 18:46:40 +0530

    remove .git

--- update-mirror.sh
+++ update-mirror.sh
@@ -6,7 +6,7 @@ BOT_TOKEN=${BOT_TOKEN}
 GITHUB_ACTOR=${BOT_NAME}
 REPOSITORY="samyakb/demo-repo-two"
 
-remote_repo="https://${GITHUB_ACTOR}:${BOT_TOKEN}@github.com/${REPOSITORY}.git"
+remote_repo="https://${GITHUB_ACTOR}:${BOT_TOKEN}@github.com/${REPOSITORY}"
 
 echo "${remote_repo}"
 git push "${remote_repo}" HEAD:main --force-with-lease
