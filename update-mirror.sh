7beb4cf17c50221ceefb1865c0baea311a466b87
Samyak Bakliwal <99634190+samyakb@users.noreply.github.com>
2022-03-16 18:55:38 +0530

    update git config

--- update-mirror.sh
+++ update-mirror.sh
@@ -9,4 +9,6 @@ REPOSITORY="samyakb/demo-repo-two"
 remote_repo="https://${GITHUB_ACTOR}:${BOT_TOKEN}@github.com/${REPOSITORY}"
 
 echo "${remote_repo}"
+git config --local user.email "samyakb@mtechzilla.com"
+git config --local user.name "Samyak Bakliwal"
 git push "${remote_repo}" HEAD:main --force-with-lease
